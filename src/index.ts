7cb7e2ea21084eb9ec3c9aa0cf5493f181548d4f
Saatvik Arya <aryasaatvik@gmail.com>
2025-01-05 22:44:46 -0800

    types: add VoyageEmbedding types to index export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { createVoyage, voyage } from './voyage-provider';
 export type { VoyageProvider, VoyageProviderSettings } from './voyage-provider';
+export type { VoyageEmbeddingModelId, VoyageEmbeddingSettings } from './voyage-embedding-settings';
